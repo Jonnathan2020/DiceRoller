ecee05c40bb63f5b50f322734d49eeb58e11058f
Jonnathan2020 <jonnathan.santos15@gmail.com>
2024-09-06 22:36:35 -0300

    Estilização do projeto já realizado, faltando o código do método

---
+++ MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿namespace DiceRoller;
+
+public partial class MainPage : ContentPage
+{
+    var picker = (Picker)sender;
+    int selectedIndex = picker.SelectedIndex;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void picker_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        var picker = (Picker)sender;
+        int selectedIndex = picker.SelectedIndex;
+
+        if (selectedIndex != -1)
+        {
+            // Obtém o item selecionado
+            string selectedItem = (string)picker.ItemsSource[selectedIndex];
+            // Atualiza o texto do Label
+            selectedItemLabel.Text = $"Você selecionou: {selectedItem}";
+        }
+        else
+        {
+            selectedItemLabel.Text = "Selecione um item";
+        }
+       
+    }
+
+    private void Sortear_Clicked(object sender, EventArgs e)
+    {
+        public virtual int Next(int minValue, int maxValue);
+    }
+
+    //Sortear um número aleatório através do clique do botão
+    //Devolver o número sorteado para a interface
+    //Conseguir selecionar a quantidade de lados a partir do Picker
+
+    //Evidenciar de acordo com cada quantidade de lados selecionado de 4, 6, 8, 10, 20, 100
+
+}
+
